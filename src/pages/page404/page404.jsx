75041294f54c3c4bca856525fd6453a624d3e615
DmitriyShvanyk <soledar1984@gmail.com>
2021-07-07 12:53:16 +0300

    Added endpoints, file utils.js

--- src/pages/page404/page404.jsx
+++ src/pages/page404/page404.jsx
@@ -1,4 +1,3 @@
-//import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './page404.module.css'
 
@@ -7,11 +6,11 @@ const Page404 = () => {
         <div className={styles.page404}>
             <div className={styles.container}>
                 <h1 className={styles.title}>
-                    Добро пожаловать на&nbsp;страницу 
+                    Добро пожаловать на&nbsp;страницу
                     <span className={styles.num}>404</span>
                 </h1>
                 <p className={styles.text}>
-                    Вы находитесь здесь, потому что ввели адрес страницы, 
+                    Вы находитесь здесь, потому что ввели адрес страницы,
                     которая уже не существует или была перемещена по другому адресу
                 </p>
                 <Link to='/' className={styles.link}>Перейти на главную страницу</Link>
